ef8100c9a4d2b84012a537a1808de5d03b0b8da2
Nicolas Umansky <umanskynico@gmail.com>
2023-12-24 16:19:13 -0300

    Refactor Supabase client initialization

--- src/index.js
+++ src/index.js
@@ -10,8 +10,8 @@ const supabaseUrl = "https://psiosmajmdqunenvdrcy.supabase.co";
 const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBzaW9zbWFqbWRxdW5lbnZkcmN5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDMwODk3MTksImV4cCI6MjAxODY2NTcxOX0.VVu53_ZzCuEfjDU-qSMl2h_CwO84Gd9_7kITJpzhKNg"
 
 const supabase = createClient(
-  "https://psiosmajmdqunenvdrcy.supabase.co",
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBzaW9zbWFqbWRxdW5lbnZkcmN5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDMwODk3MTksImV4cCI6MjAxODY2NTcxOX0.VVu53_ZzCuEfjDU-qSMl2h_CwO84Gd9_7kITJpzhKNg"
+  supabaseUrl,
+  supabaseKey
 );
 
 ReactDOM.render(
